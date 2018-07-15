d51685539eacae40e3a19ef80f56ff8ccd171a34
Valdinei Ferreira <valdineifer@outlook.com>
2018-07-15 02:56:49 -0300

    Section 12: using localStorage (html API)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,19 +3,34 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { IntroPage } from '../pages/intro/intro';
+import { ConfigProvider } from '../providers/config/config';
+import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
-  templateUrl: 'app.html'
+    templateUrl: 'app.html',
+    providers: [
+        ConfigProvider
+    ]
 })
 export class MyApp {
-  rootPage:any = IntroPage;
+    rootPage:any;
+    
+    constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, configProvider: ConfigProvider) {
+        platform.ready().then(() => {
+            // A variable that contains a config data of localStorage item
+            let config = configProvider.getConfigData();
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+            // If the item 'config' is null, then the IntroPage will be loaded and it won't be loaded anymore
+            if (config == null) {
+                this.rootPage = IntroPage;
+                configProvider.setConfigData(false);
+            } else {
+                // If 'config' is not null, it means that it's not the first access, then it will redirecly to TabsPage
+                this.rootPage = TabsPage;
+            }
+            
+            statusBar.styleDefault();
+            splashScreen.hide();
+        });
+    }
 }
---
+++ src/providers/config/config.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+// Criar esta variável para evitar conflitos
+let config_key_name = "config";
+
+@Injectable()
+export class ConfigProvider {
+    private config = {
+        showSlide: false,
+        name: "",
+        username: ""
+    };
+    
+    constructor() {
+        
+    }
+
+    getConfigData(): any {
+        return localStorage.getItem(config_key_name);
+    }
+
+    setConfigData(showSlide?: boolean, name?: string, username?: string) {
+        let config = {
+            showSlide: false,
+            name: "",
+            username: ""
+        };
+
+        if (showSlide)
+            config.showSlide = showSlide;
+
+        if (name)
+            config.name = name;
+        
+        if (username)
+            config.username = username;
+
+        localStorage.setItem(config_key_name, JSON.stringify(config));
+    }
+}
