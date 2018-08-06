4a79b325f3f82205d251baf4af76a8badc670f9c
Valdinei Ferreira <valdineifer@outlook.com>
2018-08-06 02:37:22 -0300

    Section 13: add sidemenu and clear code
    * Delete comments and useless code;
    * Sidemenu finished;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { FeedPage } from '../pages/feed/feed';
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { IntroPageModule } from '../pages/intro/intro.module';
 import { HttpClientModule } from '@angular/common/http';
-import { SettingsPageModule } from '../pages/settings/settings.module';
+import { IntroPageModule } from '../pages/intro/intro.module';
 import { ProfilePageModule } from '../pages/profile/profile.module';
+import { SettingsPageModule } from '../pages/settings/settings.module';
 
 @NgModule({
   declarations: [
